815b7463d043ea814035c7185619d96fa16e3cfa
Tim Bender <bendertj@gmail.com>
2025-10-23 10:25:33 -0400

    gitignore update for large files to be ignored

--- R/explore.R
+++ R/explore.R
@@ -3,3 +3,24 @@ library(readr)
 
 
 snapshot()
+
+rm(list=ls());cat('\f')
+
+# https://live-durhamnc.opendata.arcgis.com/search
+
+home.wd <- "C:/Users/bende/Documents/R/play/durham_open_data"
+data.wd <- "C:/Users/bende/Documents/R/play/durham_open_data/data"
+
+# rename crazy filenames
+setwd(data.wd)
+list.files()
+
+base::file.rename(
+  
+)
+
+addr <- readr::read_csv_chunked(file = "C:/Users/bende/Documents/R/play/durham_open_data/")
+
+getwd()
+
+list.files(path = "C:/Users/bende/Documents/R/play/durham_open_data/data")
